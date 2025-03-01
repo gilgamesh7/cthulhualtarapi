d82346b98b4e4c2f522f4cbdffb2465124d3b767
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-03-01 08:52:54 +0000

    Add CORS

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ API to for the cthulhualtar to consume
 # Links
 ## Local
 - [Check if alive](http://127.0.0.1:8000/api/v1/alive)
+- [Azure](https://cthulhualtar-api-begvgzh8guerb3ba.centralus-01.azurewebsites.net/api/v1/alive)
 
 ## Run
 - Local & Azure Startup Command : gunicorn -k uvicorn.workers.UvicornWorker -w 4 main:app
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://brave-rock-040a7c310.4.azurestaticapps.net"],  # Allow frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/api/v1/alive")
 def alive():
