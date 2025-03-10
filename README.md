403f06838c4a433699c481de1c46dcb09f986469
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-03-10 07:15:41 +0000

    Update readme and add localhost to the list of allowed origins

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ API to for the cthulhualtar to consume
 # Links
 ## Local
 - [Check if alive](http://127.0.0.1:8000/api/v1/alive)
-- [Azure](https://cthulhualtar-api-begvgzh8guerb3ba.centralus-01.azurewebsites.net/api/v1/alive)
+## Azure
+- [Check if alive](https://cthulhualtar-api-begvgzh8guerb3ba.centralus-01.azurewebsites.net/api/v1/alive)
 
 ## Run
 - Local & Azure Startup Command : gunicorn -k uvicorn.workers.UvicornWorker -w 4 main:app
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://brave-rock-040a7c310.4.azurestaticapps.net"],  # Allow frontend domain
+    allow_origins=["https://brave-rock-040a7c310.4.azurestaticapps.net",  # Production frontend
+                "http://localhost:3001"  ],  # Local development frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
