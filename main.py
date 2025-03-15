bff649d7d67680f8be86eca4a1667b59ff64e46c
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-03-15 06:28:14 +0000

    Change project structure again

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.config import WEATHER_API_KEY
-from app.middleware import setup_middleware
-from app.routers import health
+from config import WEATHER_API_KEY
+from middleware import setup_middleware
+from routers import health
 
 app = FastAPI()
 
